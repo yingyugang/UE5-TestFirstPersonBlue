91c4a6811a8980ffcf30c8aba5359c9303d16f77
yingyugang <yingyugang@gmail.com>
2025-06-30 17:44:53 +0900

    添加C++ 组件

---
+++ Source/TestFirstPersonBlue/TestHTTP.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestHTTP.h"
+
+TestHTTP::TestHTTP()
+{
+}
+
+TestHTTP::~TestHTTP()
+{
+}
+
+void TestHTTP::SayHello()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Hello from TestHTTP!"));
+}
---
+++ Source/TestFirstPersonBlue/TestHTTP.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TESTFIRSTPERSONBLUE_API TestHTTP
+{
+public:
+	TestHTTP();
+	~TestHTTP();
+	static void SayHello();
+};
